a6fbda72daf944a55c9df6f098919786a7f01575
ramy209873452 <61256552+ramy209873452@users.noreply.github.com>
2020-02-19 16:13:13 -0800

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+  
+  
+  document.getElementById('shawnn').style.color="green";
+  document.getElementById('shawnn').innerHTML="<h1>Thank you Shawn</h1><br>Let's do this project";
+  
+   document.getElementById('checkPage').style.display='none';
+  }, false);
+}, false);
